b64cfd7a69e63851461c59dcfad238ead72e375b
Zach Winters <zwintersdeveloper@gmail.com>
2018-01-19 14:52:59 -0500

    chart source updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TicksService } from './services/ticks.service';
 import { Chart } from 'chart.js';
+import {falseIfMissing} from 'protractor/built/util';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,53 @@ import { Chart } from 'chart.js';
 export class AppComponent {
 
   constructor(private ticks: TicksService) {}
+
+  chart: [any];
+
+  ngOnInit() {
+    this.ticks.ticks()
+      .subscribe(res => {
+
+        let price = res['ticks'].map(res => res.price);
+        let time = res['ticks'].map(res => res.timestamp);
+        let volume = res['ticks'].map(res => res.volume);
+
+        let formattedTimes = []
+        time.forEach((res) => {
+          let jstime = new Date(res * 1000);
+          formattedTimes.push(jstime.toLocaleTimeString('en',
+            {year: 'numeric', month: 'short', day: 'numeric'}
+            ));
+        });
+
+        this.chart = new Chart('canvas', {
+          type: 'scatter',
+          data: {
+            labels: formattedTimes,
+            datasets: [
+              {
+                data: price,
+                borderColor: '#3cba9f',
+                fill: false
+              },
+            ]
+          },
+          options: {
+            legend: {
+              display: false
+            },
+            scales: {
+              xAxes: [{
+                display: true
+              }],
+              yAxes: [{
+                display: true
+              }]
+            }
+          }
+        });
+      });
+
+  }
 
 }
--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -14,7 +14,6 @@ export class ChartComponent implements OnInit {
 
   ngOnInit() {
     this.http.get('/assets/json/ticks.json').subscribe(data => {
-      console.log(data);
       this.tickList = data as [any];
     });
   }
